20c5289ed9246c8b3792f5181dedfe600735c875
nikita <shtukolimp@mail.ru>
2023-03-20 18:58:09 +0300

    задача 10

---
+++ task1/Program.cs
@@ -0,0 +1,14 @@
+﻿
+//Задача ноиер 10
+Console.WriteLine("введите трехзначное число");
+int n = Convert.ToInt32(Console.ReadLine());
+int n1 = n / 100 * 100;
+int n2 = n % 10 ;
+int n3 = n - n1 - n2 ;
+Console.WriteLine($"вот второе число:   {n3 / 10}");
+
+
+
+
+
+
