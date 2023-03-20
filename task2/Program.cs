2832e986f79191d0a06bb3e5c7bec3860a73a20c
nikita <shtukolimp@mail.ru>
2023-03-20 19:14:11 +0300

    задача 15

---
+++ task2/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("введите цифру дня недели чтобы узнать выходной или нет");
+int n = Convert.ToInt32(Console.ReadLine());
+if (n > 7 || n < 1)
+Console.WriteLine("вы оштблись поробуйте сного");
+else if (n == 1)
+Console.WriteLine("net");
+else if (n == 2)
+Console.WriteLine("net");
+else if (n == 3)
+Console.WriteLine("net");
+else if (n == 4)
+Console.WriteLine("net");
+else if (n == 5)
+Console.WriteLine("net");
+else if (n == 6)
+Console.WriteLine("da");
+else
+Console.WriteLine("da");
